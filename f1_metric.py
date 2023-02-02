3be6ea9061cd8b5fc91369f1d04b5a6467d26b96
deva <aroradev314@gmail.com>
2023-02-02 09:51:34 -0600

    changed hyperparameters

--- f1_metric.py
+++ f1_metric.py
@@ -1,28 +1,13 @@
 from keras import backend as K
 
-
 def f1(y_true, y_pred):
     def recall():
-        """Recall metric.
-
-        Only computes a batch-wise average of recall.
-
-        Computes the recall, a metric for multi-label classification of
-        how many relevant items are selected.
-        """
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
         recall_ = true_positives / (possible_positives + K.epsilon())
         return recall_
 
     def precision():
-        """Precision metric.
-
-        Only computes a batch-wise average of precision.
-
-        Computes the precision, a metric for multi-label classification of
-        how many selected items are relevant.
-        """
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
         precision_ = true_positives / (predicted_positives + K.epsilon())
@@ -31,3 +16,4 @@ def f1(y_true, y_pred):
     precision = precision()
     recall = recall()
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
